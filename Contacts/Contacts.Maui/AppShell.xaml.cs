4c16ba35e91a3e321dfcc9db041b438a9679eafc
hrshuv0 <h.r.shuvo9@gmail.com>
2023-07-09 11:40:53 +0600

    Add multiple page and Routing

--- Contacts/Contacts.Maui/AppShell.xaml.cs
+++ Contacts/Contacts.Maui/AppShell.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace Contacts.Maui;
+﻿using Contacts.Maui.Views;
+
+namespace Contacts.Maui;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(ContactPage), typeof(ContactPage));
+		Routing.RegisterRoute(nameof(AddContactPage), typeof(AddContactPage));
+		Routing.RegisterRoute(nameof(EditContactPage), typeof(EditContactPage));
 	}
 }
---
+++ Contacts/Contacts.Maui/Views/ContactPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Contacts.Maui.Views;
+
+public partial class ContactPage : ContentPage
+{
+	public ContactPage()
+	{
+		InitializeComponent();
+	}
+
+    private void btnEditContact_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(EditContactPage));
+    }
+
+    private void btnAddContact_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddContactPage));
+    }
+}
